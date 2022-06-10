ab94b63d7c3b0567781d0eb151e9ddb8cc043648
MKMEK <websitedeveng@gmail.com>
2022-06-09 21:17:31 -0400

    AspectRatio
    Should not be overflowing for now

--- lib/main.dart
+++ lib/main.dart
@@ -65,74 +65,77 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
 
       //MK changed from Center to Container
-      body: Container(
-        //  alignment: Alignment.topCenter,
-        margin: new EdgeInsets.symmetric(horizontal: 30.0),
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          //   margin: EdgeInsets.only(left: 200.0, top: 300.0)),
-          children: <Widget>[
-            MaterialButton(
-                color: Colors.blue,
-                child: const Text("Pick Image from Gallery",
-                    style: TextStyle(
-                        color: Colors.white70, fontWeight: FontWeight.bold)),
-                onPressed: () {
-                  galleryImage();
-                }),
+      body: AspectRatio(
+        aspectRatio: 1 / 2,
+        child: Container(
+          //  alignment: Alignment.topCenter,
+          margin: new EdgeInsets.symmetric(horizontal: 30.0),
+          // Center is a layout widget. It takes a single child and positions it
+          // in the middle of the parent.
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            //   margin: EdgeInsets.only(left: 200.0, top: 300.0)),
+            children: <Widget>[
+              MaterialButton(
+                  color: Colors.blue,
+                  child: const Text("Pick Image from Gallery",
+                      style: TextStyle(
+                          color: Colors.white70, fontWeight: FontWeight.bold)),
+                  onPressed: () {
+                    galleryImage();
+                  }),
 
-            SizedBox(height: 50),
+              SizedBox(height: 50),
 
-            image != null ? Image.file(image!) : Text("No image selected"),
-            // Background button
-            Text(
-              'Please choose a Background/Theme',
-              style: Theme.of(context).textTheme.headline6,
-            ),
-
-            // Top caption text field
-            SizedBox(height: 70),
-            TextField(
-              //MK: TextField for input text for captions
-              //MK: the below is for wrapping the text when it finishes the a line
-              keyboardType: TextInputType.multiline,
-              minLines: 1, //Normal textInputField will be displayed
-              maxLines: null, // when user presses enter it will adapt to it
-              //MK: The below is for not showing borders, and hintText
-              decoration: InputDecoration(
-                focusedBorder: InputBorder.none,
-                border: InputBorder.none,
-                //border: OutlineInputBorder(),
-                hintText: 'Enter a Caption term',
+              image != null ? Image.file(image!) : Text("No image selected"),
+              // Background button
+              Text(
+                'Please choose a Background/Theme',
+                style: Theme.of(context).textTheme.headline6,
               ),
-            ),
 
-            SizedBox(height: 70),
-            Text('Please choose a Media file',
-                style: Theme.of(context).textTheme.headline5),
-
-            // Twitter caption field
-            SizedBox(height: 70),
-            TextField(
-              //MK: TextField for input text for Twitter captions
-              //MK: the below is for wrapping the text when it finishes the a line
-              keyboardType: TextInputType.multiline,
-              minLines: 1, //Normal textInputField will be displayed
-              maxLines: null, // when user presses enter it will adapt to it
-              //MK: The below is for not showing borders, and hintText
-              decoration: InputDecoration(
-                focusedBorder: InputBorder.none,
-                border: InputBorder.none,
-                //border: OutlineInputBorder(),
-                hintText: 'Enter a Twitter Caption term (Optional)',
+              // Top caption text field
+              SizedBox(height: 70),
+              TextField(
+                //MK: TextField for input text for captions
+                //MK: the below is for wrapping the text when it finishes the a line
+                keyboardType: TextInputType.multiline,
+                minLines: 1, //Normal textInputField will be displayed
+                maxLines: null, // when user presses enter it will adapt to it
+                //MK: The below is for not showing borders, and hintText
+                decoration: InputDecoration(
+                  focusedBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  //border: OutlineInputBorder(),
+                  hintText: 'Enter a Caption term',
+                ),
               ),
-            ),
-            //MK:Below button is for the Done/Download - Functionality to be added later
-            ButtonAnimation(
-                Color.fromRGBO(57, 92, 249, 1), Color.fromRGBO(44, 78, 233, 1)),
-          ],
+
+              SizedBox(height: 70),
+              Text('Please choose a Media file',
+                  style: Theme.of(context).textTheme.headline5),
+
+              // Twitter caption field
+              SizedBox(height: 70),
+              TextField(
+                //MK: TextField for input text for Twitter captions
+                //MK: the below is for wrapping the text when it finishes the a line
+                keyboardType: TextInputType.multiline,
+                minLines: 1, //Normal textInputField will be displayed
+                maxLines: null, // when user presses enter it will adapt to it
+                //MK: The below is for not showing borders, and hintText
+                decoration: InputDecoration(
+                  focusedBorder: InputBorder.none,
+                  border: InputBorder.none,
+                  //border: OutlineInputBorder(),
+                  hintText: 'Enter a Twitter Caption term (Optional)',
+                ),
+              ),
+              //MK:Below button is for the Done/Download - Functionality to be added later
+              ButtonAnimation(Color.fromRGBO(57, 92, 249, 1),
+                  Color.fromRGBO(44, 78, 233, 1)),
+            ],
+          ),
         ),
       ),
 
